498528ff6414783ba48bd35061fb8e6312379640
Hallyson Almeida <hallysonh@gmail.com>
2018-03-09 13:57:47 -0300

    feat: Version 1.0.0 - Integrate @hallyson/pageable with Koa by a middleware

---
+++ src/koa-pageable.ts
@@ -0,0 +1,46 @@
+import { Context } from 'koa';
+import { NumberFormatError, Pageable } from '@hallysonh/pageable';
+
+/**
+ * Converts the input into a number it it's a valid numeric string, otherwise it throws a NumberFormatError
+ *
+ * @param input Input to convert to number
+ * @returns Numeric value of string
+ */
+function parseIntOrThrow(input: string): number {
+  const result = parseInt(input, 10);
+
+  if (Number.isNaN(result)) {
+    throw new NumberFormatError(`Could not convert '${input}' to number`);
+  }
+  return result;
+}
+
+/**
+ * Converts the input to a number if it's a valid numeric string using {@link parseIntOrThrow}. If the input isn't
+ * specified, returns null
+ *
+ * @param input Input to convert to number
+ * @returns Null or numeric value of string
+ */
+function parseOptionalIntOrThrow(input?: string): number | null {
+  return input ? parseIntOrThrow(input) : null;
+}
+
+/**
+ * Koa Middleware function that reads pagination parameters from the query string, and populate `ctx.state.pageable`
+ * with a {@link Pageable} instance.
+ *
+ * @param ctx Context associated with the Koa middleware function
+ * @param next Middleware function called after {@link Pageable} property is set in state
+ * @returns {Promise}
+ */
+export async function paginate(ctx: Context, next: Function) {
+  const page = parseOptionalIntOrThrow(ctx.query.page) || 0;
+  const size = parseOptionalIntOrThrow(ctx.query.size) || 10;
+  const sort = ctx.query.sort || null;
+  const indexed: boolean = (ctx.query.indexed === 'true');
+
+  ctx.state.pageable = new Pageable(page, size, indexed, sort);
+  return next();
+}
---
+++ test/koa-pageable.test.ts
@@ -0,0 +1,50 @@
+import { Context } from 'koa';
+import { Order, Direction } from '@hallysonh/pageable';
+import { paginate } from '../src/koa-pageable';
+
+describe('Tests', () => {
+  describe('paginate function', () => {
+    const next = () => ({});
+    const context = {
+      query: {
+        page: 1,
+        size: 20,
+        sort: 'firstName',
+        indexed: 'true',
+      },
+      state: {},
+    } as Context;
+
+    it('context.state matches snapshot when paginate is called with valid context', async () => {
+      await paginate(context, next);
+      expect(context.state).toMatchSnapshot();
+    });
+
+    it('context.state matches snapshot when paginate is called with string values for page and size', async () => {
+      const updatedQuery = { ...context.query, page: '10', size: '15' };
+      await paginate({ ...context, query: updatedQuery }, next);
+      expect(context.state).toMatchSnapshot();
+    });
+
+    it('context.state matches snapshot when paginate is called with empty strings for page and size', async () => {
+      const updatedQuery = { ...context.query, page: '', size: '' };
+      await paginate({ ...context, query: updatedQuery }, next);
+      expect(context.state).toMatchSnapshot();
+    });
+
+    it('context.state matches snapshot when paginate is called with undefined page, size, and sort', async () => {
+      const updatedQuery = { indexed: 'true' };
+      await paginate({ ...context, query: updatedQuery }, next);
+      expect(context.state).toMatchSnapshot();
+    });
+
+    it('throws error when paginate is called with invalid values for page and size', async () => {
+      const updatedQuery = { ...context.query, page: 'page', size: 'size' };
+      try {
+        await paginate({ ...context, query: updatedQuery }, next);
+      } catch (e) {
+        expect(e.message).toBe("Could not convert 'page' to number");
+      }
+    });
+  });
+});
